4c15e1647e8089eda704121d10842d58c89d3fd6
zhen <zhen.yang@hotmail.fr>
2024-11-09 14:36:31 +0100

    maj

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -38,10 +38,4 @@ class TestController extends AbstractController
     {
         return $this->render('test/petitevue.html.twig');
     }
-
-    #[Route('/users-react', name: 'test_users_react')]
-    public function react(): Response
-    {
-        return $this->render('test/users_react.html.twig');
-    }
 }
