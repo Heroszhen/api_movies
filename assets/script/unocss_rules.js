9bd7b0e484eb2b478045b90cf9089f2f51af30ac
zhen <zhen.yang@hotmail.fr>
2024-12-08 15:31:16 +0100

    add customized rules

--- assets/script/unocss_rules.js
+++ assets/script/unocss_rules.js
@@ -1,6 +1,13 @@
 window.__unocss = {
-    rules: [
-       [/^bg-color-(\w+)$/, ([, color]) => ({ 'background-color': color })],
-    ],
     presets: [],
+    rules: [
+        [/^bg-color-(\w+)$/, ([, color]) => ({ 'background-color': color })],
+        [/^m-(\d+)$/, ([, size]) => ({'margin': `${size}px` })],
+        [/^m-(\w+)-(\d+)$/, ([, position, size]) => ({[`margin-${position}`]: `${size}px` })],
+        [/^p-(\d+)$/, ([, size]) => ({'padding': `${size}px` })],
+        [/^p-(\w+)-(\d+)$/, ([, position, size]) => ({[`padding-${position}`]: `${size}px` })],
+        [/^text-(\d+)$/, ([, size]) => ({'font-size': `${size}px` })],
+        [/^color-(\w+)$/, ([, color]) => ({'color': `${color}` })],
+    ],
+    shortcuts: [],
 }
--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -4,13 +4,18 @@ namespace App\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[Route('/test')]
 class TestController extends AbstractController
 {
-    public function __construct(private readonly EntityManagerInterface $entityManager)
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly HttpClientInterface $httpClient
+    )
     {}
 
     #[Route('/password', name: 'app_password')]
@@ -31,5 +36,28 @@ class TestController extends AbstractController
     public function chartjs(): Response
     {
         return $this->render('test/chartjs.html.twig');
+    }
+
+    #[Route('/unocess_rules', name: 'test_unocess_rules')]
+    public function unocess_rules(): Response
+    {
+        $countries = [];
+        try {
+            $httpClient = HttpClient::create([
+                'http_version' => '1.1',
+                'timeout' => 60
+            ]);
+            
+            $response = $httpClient->request('GET', "https://restcountries.com/v3.1/all?fields=languages,capital,area,maps,name,population,flags");
+            if (200 === $response->getStatusCode()) {
+                $countries = $response->toArray();
+            }
+        } catch (\Exception $e) {
+            dump($e->getMessage());
+        }
+
+        return $this->render('test/unocess_rules.html.twig', [
+            'countries' => $countries
+        ]);
     }
 }
